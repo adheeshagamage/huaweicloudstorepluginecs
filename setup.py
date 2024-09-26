833a2c81ecbc52007ebae7d1c91a4d4ae5a62ba3
Adheesha Gamage <adheesha.gamage@axiatadigitallabs.com>
2024-09-26 21:11:56 +0530

    version change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='huaweicloudstorepluginecs',
-    version='0.0.2',
+    version='0.0.3',
     description='Plugin that provides Huawei Cloud OBS Artifact Store functionality for MLflow',
     long_description=long_description,
     long_description_content_type="text/markdown",
